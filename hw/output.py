63f9180657df2e7d456d57b98ec078943b3b092d
Shriyanshu2004 <shriyanshurr.eai24@chanakyauniversity.edu.in>
2025-09-14 11:24:00 +0530

    Create output.py

---
+++ hw/output.py
@@ -0,0 +1,24 @@
+Enter student 1 name: A
+Enter student 2 name: B
+Enter student 3 name: C
+
+Provide marks for A:
+Math marks (0-100): 85
+Science marks (0-100): 90
+English marks (0-100): 80
+
+Provide marks for B:
+Math marks (0-100): 78
+Science marks (0-100): 88
+English marks (0-100): 92
+
+Provide marks for C:
+Math marks (0-100): 95
+Science marks (0-100): 85
+English marks (0-100): 90
+
+Name: A, Scores: {'Mathematics': 85, 'Science': 90, 'English': 80}, Total: 255  
+Name: B, Scores: {'Mathematics': 78, 'Science': 88, 'English': 92}, Total: 258  
+Name: C, Scores: {'Mathematics': 95, 'Science': 85, 'English': 90}, Total: 270  
+
+Top scorer is C with total marks: 270
